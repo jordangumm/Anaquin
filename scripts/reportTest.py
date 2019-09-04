601804240cfdb0d249a06ab6d70b6805b83cc88b
sequinstandards <47580992+sequinstandards@users.noreply.github.com>
2019-09-05 05:40:04 +1000

    Delete reportTest.py

--- scripts/reportTest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from report import *
-
-class TestRun(unittest.TestCase):
-    def test1(self):
-        self.assertTrue(len(run("whoami")) > 0)
-
-if __name__ == '__main__':
-    unittest.main()
